814c3961f2815b951573e9e11a72ba204ff17943
Santiago Miqueo <smiqueo@hotmail.com>
2021-10-26 18:27:25 -0500

    Add Prototypes

--- src/sections/PrototiposDepto/FeatureSection.js
+++ src/sections/PrototiposDepto/FeatureSection.js
@@ -9,9 +9,9 @@ import hoja1 from "~image/depto-proto/hojas-1.600.jpg"
 import hoja2 from "~image/depto-proto/hojas-2.600.jpg"
 import hoja3 from "~image/depto-proto/hojas-3.600.jpg"
 import hoja4 from "~image/depto-proto/hojas-4.600.jpg"
+import hoja5 from "~image/depto-proto/hojas-5.600.jpg"
 import hoja6 from "~image/depto-proto/hojas-6.600.jpg"
-import hoja8 from "~image/depto-proto/hojas-8.600.jpg"
-import hoja10 from "~image/depto-proto/hojas-10.600.jpg"
+import hoja7 from "~image/depto-proto/hojas-7.600.jpg"
 
 import ModalImage from "react-modal-image"
 import "react-tabs/style/react-tabs.css"
@@ -31,81 +31,54 @@ export default function FeatureSection() {
             <Tabs className="text-center">
               <TabList>
                 <Tab>
-                  <span className="titulo">95 m2 + 17.5 m2 de balcón</span>{" "}
-                  <br />{" "}
+                  <span className="titulo">Prototipo A</span> <br />{" "}
+                  <span className="descripcion">95 m2 + 17.5 m2 de balcón</span>
+                </Tab>
+                <Tab>
+                  <span className="titulo">Prototipo B</span> <br />{" "}
                   <span className="descripcion">
-                    3 Recámaras + 2 Baños + 2 Estacionamientos
+                    91.5 m2 + 17.8 m2 de balcón
                   </span>
                 </Tab>
                 <Tab>
-                  <span className="titulo">91.5 m2 + 17.8 m2 de balcón</span>{" "}
-                  <br />{" "}
+                  <span className="titulo">Prototipo C</span> <br />{" "}
+                  <span className="descripcion">80 m2 + 14.7 m2 de balcón</span>
+                </Tab>
+                <Tab>
+                  <span className="titulo">Prototipo D</span> <br />{" "}
                   <span className="descripcion">
-                    3 Recámaras + 2 Baños + 2 Estacionamientos{" "}
+                    76.4 m2 + 14.9 m2 de balcón
                   </span>
                 </Tab>
                 <Tab>
-                  <span className="titulo">80 m2 + 14.7 m2 de balcón</span>{" "}
-                  <br />{" "}
+                  <span className="titulo">Prototipo E </span> <br />{" "}
                   <span className="descripcion">
-                    2 Recámaras + 2 Baños + 1 o 2 Estacionamientos
+                    67.9 m2 + 12.5 m2 de balcón
                   </span>
                 </Tab>
                 <Tab>
-                  <span className="titulo">76.4 m2 + 14.9 m2 de balcón</span>{" "}
-                  <br />{" "}
-                  <span className="descripcion">
-                    2 Recámaras + 2 Baños + 1 o 2 Estacionamientos
-                  </span>
+                  <span className="titulo">Prototipo F</span> <br />{" "}
+                  <span className="descripcion">63.3 m2 + 12.3 m2</span>
                 </Tab>
                 <Tab>
-                  <span className="titulo"> 67.9 m2 `12.5 m2 de balcón</span>{" "}
-                  <br />{" "}
+                  <span className="titulo">Prototipo G</span> <br />{" "}
                   <span className="descripcion">
-                    2 Recámaras + 2 Baños + 1 o 2 Estacionamientos
-                  </span>
-                </Tab>
-                <Tab>
-                  <span className="titulo">63.3 m2 + 12.3 m2</span> <br />
-                  <span className="descripcion">
-                    {" "}
-                    2 Recámaras + 2 Baños + 1 Estacionamiento
-                  </span>
-                </Tab>
-                <Tab>
-                  <span className="titulo">51.7 m2 + 12.3 m2 de balcón</span>{" "}
-                  <br />{" "}
-                  <span className="descripcion">
-                    1 Recámara + 1 Baño + 1 Estacionamiento
+                    51.7 m2 + 12.3 m2 de balcón
                   </span>
                 </Tab>
               </TabList>
 
               <TabPanel>
                 <ModalImage
+                  small={hoja1}
+                  large={hoja1}
+                  className="borde-modal"
+                />
+              </TabPanel>
+              <TabPanel>
+                <ModalImage
                   small={hoja2}
                   large={hoja2}
-                  className="borde-modal"
-                />
-              </TabPanel>
-              <TabPanel>
-                <ModalImage
-                  small={hoja4}
-                  large={hoja4}
-                  className="borde-modal"
-                />
-              </TabPanel>
-              <TabPanel>
-                <ModalImage
-                  small={hoja10}
-                  large={hoja10}
-                  className="borde-modal"
-                />
-              </TabPanel>
-              <TabPanel>
-                <ModalImage
-                  small={hoja6}
-                  large={hoja6}
                   className="borde-modal"
                 />
               </TabPanel>
@@ -118,15 +91,29 @@ export default function FeatureSection() {
               </TabPanel>
               <TabPanel>
                 <ModalImage
-                  small={hoja1}
-                  large={hoja1}
+                  small={hoja4}
+                  large={hoja4}
                   className="borde-modal"
                 />
               </TabPanel>
               <TabPanel>
                 <ModalImage
-                  small={hoja8}
-                  large={hoja8}
+                  small={hoja5}
+                  large={hoja5}
+                  className="borde-modal"
+                />
+              </TabPanel>
+              <TabPanel>
+                <ModalImage
+                  small={hoja6}
+                  large={hoja6}
+                  className="borde-modal"
+                />
+              </TabPanel>
+              <TabPanel>
+                <ModalImage
+                  small={hoja7}
+                  large={hoja7}
                   className="borde-modal"
                 />
               </TabPanel>
